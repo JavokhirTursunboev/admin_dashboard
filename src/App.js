4f5cc912a3c852c8e94527f29049e635ad96497f
Javokhir <jtursunboev@bk.ru>
2023-06-13 18:02:11 +0900

    sidebar is done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from "react";
+import "./app.css";
+import Sidebar from "./components/Topbar/Sidebar/Sidebar";
 import Topbar from "./components/Topbar/Topbar";
-
 
 const App = () => {
   return (
     <>
       <Topbar />
+      <div className="container">
+        <Sidebar />
+        <div className="other"> other pages</div>
+      </div>
     </>
   );
 };

db8ef9a7f67b05dac6719c481c9f623611b1e0ca
Javokhir <jtursunboev@bk.ru>
2023-06-22 19:11:26 +0900

    widget small is done

--- src/components/widgetSm/WidgetSm.jsx
+++ src/components/widgetSm/WidgetSm.jsx
@@ -1,7 +1,89 @@
+import { Visibility } from "@material-ui/icons";
 import React from "react";
 import "./widgetSm.css";
 const WidgetSm = () => {
-  return <div className="widgetSm">WidgetSm</div>;
+  return (
+    <div className="widgetSm">
+      <span className="widgetSmTitle">New Join Memebers</span>
+      <ul className="widgetSmList">
+        <li className="widgetSmListItem">
+          <img
+            src="https://images.pexels.com/photos/878358/pexels-photo-878358.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            className="widgetSmImg"
+            alt="img"
+          />
+          <div className="widgetSmUser">
+            <span className="widgetSmUserName">Jack Smth</span>
+            <span className="widgetSmUserTitle">Software Engineer</span>
+          </div>
+          <button className="widgetSmButton">
+            <Visibility className="widgetSmIcon" />
+            Display
+          </button>
+        </li>
+        <li className="widgetSmListItem">
+          <img
+            src="https://images.pexels.com/photos/878358/pexels-photo-878358.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            className="widgetSmImg"
+            alt="img"
+          />
+          <div className="widgetSmUser">
+            <span className="widgetSmUserName">Jack Smth</span>
+            <span className="widgetSmUserTitle">Software Engineer</span>
+          </div>
+          <button className="widgetSmButton">
+            <Visibility className="widgetSmIcon" />
+            Display
+          </button>
+        </li>
+        <li className="widgetSmListItem">
+          <img
+            src="https://images.pexels.com/photos/878358/pexels-photo-878358.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            className="widgetSmImg"
+            alt="img"
+          />
+          <div className="widgetSmUser">
+            <span className="widgetSmUserName">Jack Smth</span>
+            <span className="widgetSmUserTitle">Software Engineer</span>
+          </div>
+          <button className="widgetSmButton">
+            <Visibility className="widgetSmIcon" />
+            Display
+          </button>
+        </li>
+        <li className="widgetSmListItem">
+          <img
+            src="https://images.pexels.com/photos/878358/pexels-photo-878358.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            className="widgetSmImg"
+            alt="img"
+          />
+          <div className="widgetSmUser">
+            <span className="widgetSmUserName">Jack Smth</span>
+            <span className="widgetSmUserTitle">Software Engineer</span>
+          </div>
+          <button className="widgetSmButton">
+            <Visibility className="widgetSmIcon" />
+            Display
+          </button>
+        </li>
+        <li className="widgetSmListItem">
+          <img
+            src="https://images.pexels.com/photos/878358/pexels-photo-878358.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            className="widgetSmImg"
+            alt="img"
+          />
+          <div className="widgetSmUser">
+            <span className="widgetSmUserName">Jack Smth</span>
+            <span className="widgetSmUserTitle">Software Engineer</span>
+          </div>
+          <button className="widgetSmButton">
+            <Visibility className="widgetSmIcon" />
+            Display
+          </button>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default WidgetSm;
